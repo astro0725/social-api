1230b17fc24140e12ef41e26c589949abbb7fc86
astro <angelica.strong0725@gmail.com>
2024-02-14 21:17:17 -0800

    get all user function

--- controllers/user.js
+++ controllers/user.js
@@ -1,15 +1,12 @@
-// logic for user routes
-// **`/api/users`**
+const { User, Thought } = require('../models');
 
-// * `GET` all users
+// get all users
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find().populate('thoughts').populate('friends');
+    res.json(users);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
 
-// * `GET` a single user by its `_id` and populated thought and friend data
-
-// * `POST` a new user:
-
-
-// * `PUT` to update a user by its `_id`
-
-// * `DELETE` to remove user by its `_id`
-
-// **BONUS**: Remove a user's associated thoughts when deleted.
