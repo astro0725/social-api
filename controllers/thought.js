38c9baa1aaaedc731900849f2dba8cb44659d302
astro <angelica.strong0725@gmail.com>
2024-02-14 21:25:44 -0800

    get all thoughts

--- controllers/thought.js
+++ controllers/thought.js
@@ -1,12 +1,11 @@
-// logic for thought routes
-// **`/api/thoughts`**
+const { User, Thought } = require('../models');
 
-// * `GET` to get all thoughts
-
-// * `GET` to get a single thought by its `_id`
-
-// * `POST` to create a new thought (don't forget to push the created thought's `_id` to the associated user's `thoughts` array field)
-
-// * `PUT` to update a thought by its `_id`
-
-// * `DELETE` to remove a thought by its `_id`
+// get all thoughts
+exports.getAllThoughts = async (req, res) => {
+  try {
+    const thoughts = await Thought.find();
+    res.json(thoughts);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
