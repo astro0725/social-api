4863a54cd995fcafd476cca1d670d19080f02d34
astro <angelica.strong0725@gmail.com>
2024-02-14 21:24:05 -0800

    moved to different file to improve modularity

--- controllers/friend.js
+++ controllers/friend.js
@@ -1,6 +1,35 @@
-// logic for /user/friend routes
-// **`/api/users/:userId/friends/:friendId`**
+const { User } = require('../models');
 
-// * `POST` to add a new friend to a user's friend list
+// add a friend to a user's friend list
+exports.addFriend = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.params.userId,
+      { $addToSet: { friends: req.params.friendId } }, 
+      { new: true }
+    );
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
 
-// * `DELETE` to remove a friend from a user's friend list
+// remove a friend from a user's friend list
+exports.removeFriend = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.params.userId,
+      { $pull: { friends: req.params.friendId } },
+      { new: true }
+    );
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
--- controllers/user.js
+++ controllers/user.js
@@ -60,37 +60,3 @@ exports.deleteUser = async (req, res) => {
     res.status(500).send(error.message);
   }
 };
-
-// add a friend to a user's friend list
-exports.addFriend = async (req, res) => {
-  try {
-    const user = await User.findByIdAndUpdate(
-      req.params.userId,
-      { $addToSet: { friends: req.params.friendId } }, 
-      { new: true }
-    );
-    if (!user) {
-      return res.status(404).send('User not found');
-    }
-    res.json(user);
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
-};
-
-// remove a friend from a user's friend list
-exports.removeFriend = async (req, res) => {
-  try {
-    const user = await User.findByIdAndUpdate(
-      req.params.userId,
-      { $pull: { friends: req.params.friendId } },
-      { new: true }
-    );
-    if (!user) {
-      return res.status(404).send('User not found');
-    }
-    res.json(user);
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
-};
