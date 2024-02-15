0703bd80c2b0d7dafd688884cafbecc90939e7a6
astro <angelica.strong0725@gmail.com>
2024-02-14 21:30:47 -0800

    add/remove reaction

--- controllers/reaction.js
+++ controllers/reaction.js
@@ -1,6 +1,35 @@
-// logic for thought/reaction routes
-// **`/api/thoughts/:thoughtId/reactions`**
+const { Thought } = require('../models');
 
-// * `POST` to create a reaction stored in a single thought's `reactions` array field
+// add a reaction to a thought
+exports.addReaction = async (req, res) => {
+  try {
+    const thought = await Thought.findByIdAndUpdate(
+      req.params.thoughtId,
+      { $push: { reactions: req.body } },
+      { new: true, runValidators: true }
+    );
+    if (!thought) {
+      return res.status(404).send('Thought not found');
+    }
+    res.json(thought);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
 
-// * `DELETE` to pull and remove a reaction by the reaction's `reactionId` value
+// remove a reaction from a thought
+exports.removeReaction = async (req, res) => {
+  try {
+    const thought = await Thought.findByIdAndUpdate(
+      req.params.thoughtId,
+      { $pull: { reactions: { reactionId: req.params.reactionId } } },
+      { new: true }
+    );
+    if (!thought) {
+      return res.status(404).send('Thought not found');
+    }
+    res.json(thought);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
