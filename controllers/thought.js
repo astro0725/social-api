51444530f9706eaadec4cc7a66649007a97da38a
astro <angelica.strong0725@gmail.com>
2024-02-14 21:26:13 -0800

    get thought by id

--- controllers/thought.js
+++ controllers/thought.js
@@ -9,3 +9,16 @@ exports.getAllThoughts = async (req, res) => {
     res.status(500).send(error.message);
   }
 };
+
+// get a single thought by _id
+exports.getThoughtById = async (req, res) => {
+  try {
+    const thought = await Thought.findById(req.params.id);
+    if (!thought) {
+      return res.status(404).send('Thought not found');
+    }
+    res.json(thought);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
